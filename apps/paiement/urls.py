9c1028a615487c43cb47f264bfe64a7b3f7b647a
Oumar <omatest80@gmail.com>
2023-01-17 21:27:08 +0000

    Receipt print implemented

--- apps/paiement/urls.py
+++ apps/paiement/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("payments/", views.payments_view, name="payments"),
     path("payments/add/", views.add_payment_view, name="add_payment"),
     path("payments/<int:payment_id>/edit", views.edit_payment_view, name="edit_payment"),
+    path("payments/<int:payment_id>/print", views.payment_receipt_view, name="payment_receipt"),
     path("devises/", views.devises_view, name="devises"),
     path("devises/update", views.devises_update_view, name="update_devises"),
     path("devises/<int:permit_id>/<int:devise_id>/value", views.get_devise_value, name="get_devise_value"),

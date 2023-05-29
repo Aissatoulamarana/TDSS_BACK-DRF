09ab3bb259256e69e1cc304724fc0dcda8d27c98
Oumar <omatest80@gmail.com>
2023-05-29 14:28:25 +0000

    Profile forms icons updated and picture upload/display handled

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -88,7 +88,7 @@ class Profile(models.Model):
     contact = models.IntegerField(blank=True, null=True)
     account = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name="profile_account")
     status = models.CharField(max_length=30, choices=[('ON', "Actif"), ('OFF', "Inactif")], default="ON")
-    picture = models.ImageField(upload_to="staticfiles", blank=True, null=True, verbose_name="profile picture")
+    picture = models.ImageField(upload_to="profile_pictures", blank=True, null=True, verbose_name="profile picture")
     email = models.EmailField(max_length=100, blank=True, null=True)
     adresse = models.TextField(max_length=255, blank=True, null=True)
     created_by = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name="profile_creator")
--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,18 @@ Copyright (c) 2022 - OD
 from django.contrib import admin
 from django.urls import path, include  # add this
 
-urlpatterns = [
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+urlpatterns += [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.paiement.urls")),
     path("", include("apps.home.urls")),             # UI Kits Html files
 ]
+ 

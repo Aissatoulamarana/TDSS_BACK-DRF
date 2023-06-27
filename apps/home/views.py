21f6890519617634c42c2597efc52f4b45c8af69
Oumar <omatest80@gmail.com>
2023-06-27 12:58:42 +0000

    Dashboard updated

--- apps/home/views.py
+++ apps/home/views.py
@@ -22,7 +22,7 @@ devises = Devise.objects.all().order_by('id')
 @login_required(login_url="/login/")
 def index(request):
 
-    if request.user.profile.type.uid == 1:
+    if request.user.profile.type.uid == 1 or request.user.profile.type.uid == 3:
         context = {
             'segment': 'index',
             'taux': devises,
@@ -40,22 +40,40 @@ def index(request):
             'paiement_USD': Payment.objects.filter(devise=Devise.objects.get(pk=2)).aggregate(Sum('amount'))['amount__sum']
         }
     else:
-        context = {
-            'segment': 'index',
-            'taux': devises,
-            'total_users': CustomUser.objects.filter(created_by=request.user).order_by('-created_on'),
-            'total_profiles': Profile.objects.all().order_by('-created_on'),
-            'total_agencies': Agency.objects.filter(created_by=request.user),
-            'agencies_conakry': Agency.objects.filter(region=Region.objects.get(code='GN-C'), created_by=request.user),
-            'agencies_interior': Agency.objects.exclude(region=Region.objects.get(code='GN-C'), created_by=request.user),
+        if request.user.type.uid == 1 or request.user.type.uid == 3:
+            context = {
+                'segment': 'index',
+                'taux': devises,
+                'total_users': CustomUser.objects.filter(created_by__profile=request.user.profile).order_by('-created_on'),
+                'total_profiles': Profile.objects.all().order_by('-created_on'),
+                'total_agencies': Agency.objects.filter(created_by__profile=Profile.objects.first()),
+                'agencies_conakry': Agency.objects.filter(region=Region.objects.get(code='GN-C'), created_by__profile=request.user.profile),
+                'agencies_interior': Agency.objects.exclude(region=Region.objects.get(code='GN-C')).filter(created_by__profile=request.user.profile),
 
-            'total_declarations': Declaration.objects.filter(created_by=request.user).order_by('-created_on'),
-            'total_employees': Employee.objects.filter(declaration__created_by=request.user),
-            'total_factures': Facture.objects.filter(created_by=request.user).order_by('-created_on'),
-            'total_paiements': Payment.objects.filter(created_by=request.user).order_by('-created_on'),
-            'paiement_GNF': Payment.objects.filter(devise=Devise.objects.get(pk=1), created_by=request.user).aggregate(Sum('amount'))['amount__sum'],
-            'paiement_USD': Payment.objects.filter(devise=Devise.objects.get(pk=2), created_by=request.user).aggregate(Sum('amount'))['amount__sum']
-        }
+                'total_declarations': Declaration.objects.filter(created_by__profile=request.user.profile).order_by('-created_on'),
+                'total_employees': Employee.objects.filter(declaration__created_by__profile=request.user.profile),
+                'total_factures': Facture.objects.filter(created_by__profile=request.user.profile).order_by('-created_on'),
+                'total_paiements': Payment.objects.filter(created_by__profile=request.user.profile).order_by('-created_on'),
+                'paiement_GNF': Payment.objects.filter(devise=Devise.objects.get(pk=1), created_by__profile=request.user.profile).aggregate(Sum('amount'))['amount__sum'],
+                'paiement_USD': Payment.objects.filter(devise=Devise.objects.get(pk=2), created_by__profile=request.user.profile).aggregate(Sum('amount'))['amount__sum']
+            }
+        else:
+            context = {
+                'segment': 'index',
+                'taux': devises,
+                'total_users': CustomUser.objects.filter(created_by=request.user).order_by('-created_on'),
+                'total_profiles': Profile.objects.all().order_by('-created_on'),
+                'total_agencies': Agency.objects.filter(created_by=request.user),
+                'agencies_conakry': Agency.objects.filter(region=Region.objects.get(code='GN-C'), created_by=request.user),
+                'agencies_interior': Agency.objects.exclude(region=Region.objects.get(code='GN-C')).filter(created_by=request.user),
+
+                'total_declarations': Declaration.objects.filter(created_by=request.user).order_by('-created_on'),
+                'total_employees': Employee.objects.filter(declaration__created_by=request.user),
+                'total_factures': Facture.objects.filter(created_by=request.user).order_by('-created_on'),
+                'total_paiements': Payment.objects.filter(created_by=request.user).order_by('-created_on'),
+                'paiement_GNF': Payment.objects.filter(devise=Devise.objects.get(pk=1), created_by=request.user).aggregate(Sum('amount'))['amount__sum'],
+                'paiement_USD': Payment.objects.filter(devise=Devise.objects.get(pk=2), created_by=request.user).aggregate(Sum('amount'))['amount__sum']
+            }
 
     # context = {
     #     'segment': 'index',
